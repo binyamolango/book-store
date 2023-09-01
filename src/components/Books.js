8cd0a83d2e8238671e4acf36580ec8121ec4f803
binyamolango <olangobinyam@gmail.com>
2023-09-01 06:07:33 +0300

    Add books and catagories components

---
+++ src/components/Books.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+const Books = (props) => {
+  const { title, author } = props;
+
+  return (
+    <li>
+      <p>Book title :{title}</p>
+      <div>
+        <p>Author :{author}</p>
+        <button type="button">Remove</button>
+      </div>
+    </li>
+  );
+};
+
+Books.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+export default Books;
