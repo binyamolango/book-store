9695c850bce8ff25a45020dcb9426bfd94991ab3
binyamolango <olangobinyam@gmail.com>
2023-09-06 11:23:00 +0300

    Add remove button and onclick function on it

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,36 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const booksSlice = createSlice({
-  name: 'books',
-  initialState: [
+const initialState = {
+  books: [
     {
-      item_id: 'item1',
+      id: '1',
       title: 'The Great Gatsby',
       author: 'John Smith',
       category: 'Fiction',
     },
     {
-      item_id: 'item2',
+      id: '2',
       title: 'Anna Karenina',
       author: 'Leo Tolstoy',
       category: 'Fiction',
     },
     {
-      item_id: 'item3',
+      id: '3',
       title: 'The Selfish Gene',
       author: 'Richard Dawkins',
       category: 'Nonfiction',
     },
   ],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload);
+      state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.id !== action.payload);
     },
   },
 });
 
-export default booksSlice.reducer;
 export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
