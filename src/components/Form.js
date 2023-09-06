df9aab5a9124176d508727f1d896b5ae5e818b9a
binyamolango <olangobinyam@gmail.com>
2023-09-06 12:10:19 +0300

    Add add button click hander

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,20 @@
-const Form = () => (
-  <form>
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Author" />
-    <button type="submit">ADD BOOk</button>
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
+const Form = () => {
+  const dispatch = useDispatch();
+
+  const addClickHandler = () => {
+    dispatch(addBook());
+  };
+
+  return (
+    <form>
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Author" />
+      <button type="submit" onClick={addClickHandler}>ADD BOOk</button>
+    </form>
+  );
+};
 
 export default Form;
